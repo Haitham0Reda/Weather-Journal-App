6b48c6ed6a3053762abafc1cd854210178d5e65a
Haitham0Reda <102927584+Haitham0Reda@users.noreply.github.com>
2022-07-30 01:14:25 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = [];
+// Express to run server and routes
+const express = require("express");
+// Start up an instance of app
+const app = express();
+/* Dependencies */
+const bodyParser = require("body-parser");
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static("website"));
+
+const port = 8080;
+// Spin up the server
+const server = app.listen(port, listening);
+
+// Callback to debug
+function listening() {
+  console.log(`server running on localhost: ${port}`);
+}
+// Initialize all route with a callback function
+
+app.get("/allApiData", sendData);
+
+// Callback function to complete GET '/all'
+function sendData(request, response) {
+  response.send(projectData);
+}
+// Post Route
+app.post("/addToApiData", addData);
+
+function addData(req, res) {
+  let newData = {
+    date: req.body.date,
+    temp: req.body.temp,
+    content: req.body.content,
+  };
+  console.log(req.body);
+  projectData.push(newData);
+}
---
+++ website/app.js
@@ -0,0 +1,80 @@
+const { json } = require("body-parser");
+
+//Global Variables
+let generateBtn = document.getElementById("generate");
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+// Personal API Key for OpenWeatherMap API
+const BaseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip='
+const APIKey = "&appid=36748a7a900fe3e1b2e42d1832dee4e7&units=metric";
+let zipCode = document.getElementById("zip").value;
+let feelings = document.getElementById("feelings").value;
+
+
+// Event listener to add function to existing HTML DOM element
+generateBtn.addEventListener("click", preFetch);
+/* Function called by event listener */
+function preFetch() {
+  showWeather(BaseUrl, zipCode, APIKey)
+    .then((data) => {
+      postApiData("/addToApiData", { temp: data.main.temp, date: newDate, content: content })
+    })
+    .then(() => {
+      UiData()
+    })
+}
+
+
+/* Function to GET Web API Data*/
+const showWeather = async (BaseUrl, zipCode, APIKey) => {
+  const res = await fetch(BaseUrl + zipCode + APIKey)
+  try {
+    const data = await res.json();
+    return data;
+    console.log(data);
+  } catch (error) {
+    console.log('error', error);
+  }
+
+
+  /* Function to POST data */
+  const postApiData = async(url = '', data = {})()=> {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      temp: data.temp,
+      date: data.date,
+      content: data.content
+    })
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+/* Function to GET Project Data */
+const UiData = async () => {
+  const request = await fetch('/allApiData');
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    // update new entry values
+    if (allData.date !== undefined && allData.temp !== undefined && allData.content !== undefined) {
+      document.getElementById('date').innerHTML = allData.date;
+      document.getElementById('temp').innerHTML = allData.temp + ' degree C';
+      document.getElementById('content').innerHTML = allData.content;
+    }
+  } catch (error) {
+    console.log('error', error);
+  }
+};
